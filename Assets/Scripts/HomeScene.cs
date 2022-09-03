451e64c4738a91eda0deca47858297ee7576f10c
Invokeviolet <cpflskfn@gmail.com>
2022-09-04 06:48:37 +0900

    ++Scene

--- Assets/Scripts/HomeScene.cs
+++ Assets/Scripts/HomeScene.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HomeScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static public void Start()
     {
-        
+        GameObject.Find("Next Button").GetComponentInChildren<Text>().text = "Next";
+        GameObject.Find("Home Button").GetComponentInChildren<Text>().text = "Home";
+    }
+    static public void Level1()
+    {
+        SceneManager.LoadScene("InGame1");
+    }
+    static public void Level2()
+    {
+        SceneManager.LoadScene("InGame2");
+    }
+    static public void Level3()
+    {
+        SceneManager.LoadScene("InGame3");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Home()
     {
-        
+        SceneManager.LoadScene("GameHome");
     }
 }
