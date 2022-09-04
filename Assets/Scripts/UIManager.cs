2f5ce15d7bb482e87efcbcdc07b1fd5afb8996c1
Invokeviolet <cpflskfn@gmail.com>
2022-09-05 06:22:34 +0900

    ++UIManager
    UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -18,8 +18,11 @@ public class UIManager : MonoBehaviour
     //HUD캔버스
     //WaveText 빈오브젝트 생성
     //
-    //사용된 총알 갯수 / 최대 총알 갯수 표시되는 곳 -> 마우스 커서 오른쪽 아래, 왼쪽 상단 총알
-
+    // 마우스 커서 오른쪽 아래, 왼쪽 상단 총알
+    void UseBulletCount() //사용된 총알 갯수 / 최대 총알 갯수 표시
+    { 
+    
+    }
 
     private void Awake()
     {
